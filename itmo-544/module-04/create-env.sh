055dff12ece662b5a7bd46c41a2a08454bbab61c
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-12 21:27:19 -0500

    Updated create-env.sh

---
+++ itmo-544/module-04/create-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
+
+aws ec2 run-instances --image-id ${1} --instance-type ${2}
+    --key-name ${3} --security-group-ids ${4}
+    --count ${5} --user-data file://${6}
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=itmo-544,Value=module-04}]'
+    --placement "AvailabilityZone=${7}"
