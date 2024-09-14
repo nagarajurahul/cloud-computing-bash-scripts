d93c14da59e05b434ce149df1e892aa513390e5c
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-14 17:25:26 -0500

    updated arguments.txt

--- itmo-544/module-04/create-env.sh
+++ itmo-544/module-04/create-env.sh
@@ -5,5 +5,5 @@
 aws ec2 run-instances --image-id ${1} --instance-type ${2}
     --key-name ${3} --security-group-ids ${4}
     --count ${5} --user-data file://${6}
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=itmo-544,Value=module-04}]'
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}]'
     --placement "AvailabilityZone=${7}"
