b50f2e79176906029baf458b732c6f49d40c2121
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-17 14:06:56 -0500

    commented run-instances command in create-env.sh

--- itmo-544/module-04/create-env.sh
+++ itmo-544/module-04/create-env.sh
@@ -4,15 +4,15 @@
 
 # Creating instances and running them
 
-aws ec2 run-instances \
-    --image-id ${1} \
-    --instance-type ${2} \
-    --key-name ${3} \
-    --security-group-ids ${4} \
-    --count ${5} \
-    --user-data file://${6} \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=cli-instance-launch1}]' \
-    --placement "AvailabilityZone=${7}" \
+# aws ec2 run-instances \
+#     --image-id ${1} \
+#     --instance-type ${2} \
+#     --key-name ${3} \
+#     --security-group-ids ${4} \
+#     --count ${5} \
+#     --user-data file://${6} \
+#     --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=cli-instance-launch1}]' \
+#     --placement "AvailabilityZone=${7}" \
 
 # Finding subnets
 
@@ -26,9 +26,9 @@ echo $SUBNET2A
 echo $SUBNET2B
 echo $SUBNET2C
 
-# aws elbv2 create-load-balancer \
-#     --name ${8} \
-#     --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
-#     --tags Key='name',Value=${13} 
+aws elbv2 create-load-balancer \
+    --name ${8} \
+    --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
+    --tags 'Key=course,Value=${13}'
 
 #     --scheme internal \  -> to make it internal LB
