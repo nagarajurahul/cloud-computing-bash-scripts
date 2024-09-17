002396bd3faee600df0a1cff927a2fcc69b09e71
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-17 12:04:41 -0500

    updated arguments to have comments and edited destroy.sh

--- itmo-544/module-04/destroy-env.sh
+++ itmo-544/module-04/destroy-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/terminate-instances.html
 
 aws ec2 terminate-instances --instance-ids ${1}
