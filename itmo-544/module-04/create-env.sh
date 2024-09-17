538772255f121a21d1772015b15dfd3f75eeb775
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-17 12:07:39 -0500

    updated create-env.sh to create subnets

--- itmo-544/module-04/create-env.sh
+++ itmo-544/module-04/create-env.sh
@@ -2,8 +2,27 @@
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
 
+# Creating instances and running them
+
 aws ec2 run-instances --image-id ${1} --instance-type ${2} \
     --key-name ${3} --security-group-ids ${4} \
     --count ${5} --user-data file://${6} \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}]' \
     --placement "AvailabilityZone=${7}" \
+
+# Finding subnets
+
+echo "Finding and storing the subnet IDs for defined in arguments.txt Availability Zone 1 and 2..."
+
+SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${10}")
+SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${11}")
+SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
+
+echo $SUBNET2A
+echo $SUBNET2B
+echo $SUBNET2C
+
+aws elbv2 create-load-balancer \
+    --name ${8} \
+    --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
+    --tags Key='name',Value=${13} 
