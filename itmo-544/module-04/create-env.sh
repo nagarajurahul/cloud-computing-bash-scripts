0124ff3c4b858e4dd1823d67bc8e768173331d9e
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-19 09:31:11 -0500

    updated to add wait commands

--- itmo-544/module-04/create-env.sh
+++ itmo-544/module-04/create-env.sh
@@ -14,6 +14,17 @@ aws ec2 run-instances \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=cli-instance-launch1}]' \
     --placement "AvailabilityZone=${7}" \
 
+EC2IDS=$(aws ec2 describe-instances \
+    --output=text \
+    --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running)
+
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/instance-running.html
+
+echo "Waiting for instances..."
+aws ec2 wait instance-running --instance-ids $EC2IDS
+echo "Instances are up!"
+
 # Finding subnets
 
 echo "Finding and storing the subnet IDs for defined in arguments.txt Availability Zone 1 and 2..."
@@ -21,14 +32,31 @@ echo "Finding and storing the subnet IDs for defined in arguments.txt Availabili
 SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${10}")
 SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${11}")
 SUBNET2C=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
-    
+
 echo $SUBNET2A
 echo $SUBNET2B
 echo $SUBNET2C
 
-# aws elbv2 create-load-balancer \
-#     --name ${8} \
-#     --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
-#     --tags Key=course,Value=${13}
+aws elbv2 create-load-balancer \
+    --name ${8} \
+    --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
+    --tags Key=course,Value=${13}
 
 #     --scheme internal \  -> to make it internal LB
+
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
+
+echo "Finding and storing the EL ARNS for default region"
+
+ELBARNS=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
+
+echo "*********************************************************************************************"
+echo $ELBARNS
+echo "*********************************************************************************************"
+
+# add elv2 wait running reference
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/wait/
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
+echo "Waiting for ELB to become available..."
+aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
+echo "ELB is available..."
