fec4c56c34ee5bdaa72c3d2350449f98cf471723
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-24 08:50:06 -0500

    updated order of instances and loadbalancer in create script

--- itmo-544/module-04/create-env.sh
+++ itmo-544/module-04/create-env.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
-
-# Creating instances and running them
-
-aws ec2 run-instances \
-    --image-id ${1} \
-    --instance-type ${2} \
-    --key-name ${3} \
-    --security-group-ids ${4} \
-    --count ${5} \
-    --user-data file://${6} \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=cli-instance-launch1}]' \
-    --placement "AvailabilityZone=${7}" \
-
-EC2IDS=$(aws ec2 describe-instances \
-    --output=text \
-    --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running)
-echo "Instance IDs"
-echo $EC2IDS
-
-#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/
-#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/instance-running.html
-
-echo "Waiting for instances..."
-aws ec2 wait instance-running --instance-ids $EC2IDS
-echo "Instances are up!"
-
 # Finding subnets
 
 echo "Finding and storing the subnet IDs for defined in arguments.txt Availability Zone 1 and 2..."
@@ -62,3 +35,31 @@ echo "**************************************************************************
 echo "Waiting for ELB to become available..."
 aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARNS
 echo "ELB is available..."
+
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
+
+# Creating instances and running them
+
+aws ec2 run-instances \
+    --image-id ${1} \
+    --instance-type ${2} \
+    --key-name ${3} \
+    --security-group-ids ${4} \
+    --count ${5} \
+    --user-data file://${6} \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=cli-instance-launch1}]' \
+    --placement "AvailabilityZone=${7}" \
+
+EC2IDS=$(aws ec2 describe-instances \
+    --output=text \
+    --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running)
+echo "Instance IDs"
+echo $EC2IDS
+
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/instance-running.html
+
+echo "Waiting for instances..."
+aws ec2 wait instance-running --instance-ids $EC2IDS
+echo "Instances are up!"
