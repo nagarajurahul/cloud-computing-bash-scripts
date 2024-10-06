31cb793371025a6ee572e4c6158de27c17232e63
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-10-06 16:04:05 -0500

    New file added

---
+++ git-automate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Navigate to the project directory (modify the path accordingly)
+cd /home/vagrant/rnagaraju
+
+echo "Working directory: $(pwd)"
+
+# Check if there are any changes to add
+#if git diff-index --quiet HEAD --; then
+
+# Check if there are any changes (including untracked files)
+if [[ -z $(git status --porcelain) ]]; then
+    echo "No changes to commit."
+else
+    # Add all changes to the staging area
+    git add -A
+
+    # Prompt the user for a commit message
+    echo "Enter the commit message:"
+    read commitMessage
+
+    # Commit the changes
+    git commit -m "$commitMessage"
+
+    # Push the changes to the main branch
+    if git push origin main; then
+        echo "Push successful."
+    else
+        echo "Push failed."
+    fi
+fi
+
