9a90cc4920aba9300f803a8fedc4819043083e60
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-10-15 09:28:59 -0500

    created new directory module-05

---
+++ itmo-544/module-05/create-env.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+# Finding subnets
+
+echo "Finding and storing the subnet IDs for availability zones defined in arguments"
+
+SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${10}")
+SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${11}")
+SUBNET2C=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
+
+echo "*********************************************************************************************"
+echo $SUBNET2A
+echo $SUBNET2B
+echo $SUBNET2C
+echo "*********************************************************************************************"
+
+echo "*********************************************************************************************"
+echo "Creating load-balancer now..."
+
+aws elbv2 create-load-balancer \
+    --name ${8} \
+    --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
+    --tags Key=course,Value=${13} \
+    --security-groups ${15}\
+    --output table
+
+#     --scheme internal \  -> to make it internal LB
+
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
+
+echo "Finding and storing the ELB ARN for default region"
+
+ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
+
+echo "*********************************************************************************************"
+echo $ELBARN
+echo "*********************************************************************************************"
+
+
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/wait/
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
+
+echo "*********************************************************************************************"
+echo "Waiting for ELB to become available..."
+aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
+echo "ELB is available..."
+
+echo "*********************************************************************************************"
+echo "Creating EC2 instances now..."
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html
+
+# Creating instances and running them
+
+aws ec2 run-instances \
+    --image-id ${1} \
+    --instance-type ${2} \
+    --key-name ${3} \
+    --security-group-ids ${4} \
+    --count ${5} \
+    --user-data file://${6} \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=cli-instance-launch1}]' \
+    --placement "AvailabilityZone=${7}" \
+    --output table
+
+EC2IDS=$(aws ec2 describe-instances \
+    --output=text \
+    --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running)
+
+echo "Finding and storing the Instance IDs"
+echo "*********************************************************************************************"
+echo $EC2IDS
+echo "*********************************************************************************************"
+
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/instance-running.html
+
+echo "*********************************************************************************************"
+echo "Waiting for instances..."
+aws ec2 wait instance-running --instance-ids $EC2IDS
+echo "Instances are up!"
+
+
+echo "*********************************************************************************************"
+echo "Creating target groups now..."
+
+# Find the VPC
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-vpcs.html
+
+MYVPCID=$(aws ec2 describe-vpcs --output=text --query='Vpcs[*].VpcId')
+
+# Create the target group - this is application load balancer type
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/create-target-group.html
+
+aws elbv2 create-target-group \
+    --name ${9} \
+    --protocol HTTP \
+    --port 80 \
+    --target-type instance \
+    --vpc-id $MYVPCID
+
+# Describe the target groups and get target ARNs
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-target-groups.html
+
+echo "Finding and storing target group ARN"
+
+TGARN=$(aws elbv2 describe-target-groups --output=text --query='TargetGroups[*].TargetGroupArn' --names ${9})
+
+echo "*********************************************************************************************"
+echo $TGARN
+echo "*********************************************************************************************"
+
+echo "*********************************************************************************************"
+echo "Creating listeners now..."
+
+# Creating listener
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/create-listener.html
+
+aws elbv2 create-listener \
+    --load-balancer-arn $ELBARN \
+    --protocol HTTP \
+    --port 80 \
+    --default-actions Type=forward,TargetGroupArn=$TGARN
+
+echo "Listeners are up!"
+
+# Register targets and wait for them to be in service
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/register-targets.html
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/target-in-service.html
+
+# Create a bash Array so we can loop through it and take care of the Id
+
+declare -a IDSARRAY
+IDSARRAY=( $EC2IDS )
+
+echo "*********************************************************************************************"
+
+for ID in ${IDSARRAY[@]};
+do
+  
+  echo "Registering Target with Id: $ID"
+  aws elbv2 register-targets \
+    --target-group-arn $TGARN --targets Id=$ID,Port=80
+  aws elbv2 wait target-in-service  --target-group-arn $TGARN --targets Id=$ID,Port=80
+  echo "Target $ID is in service!!!"
+  
+done
+
+echo "*********************************************************************************************"
+
+DNSNAME=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].DNSName')
+DNSNAME="http://$DNSNAME"
+
+echo "Kindly curl using the below link"
+echo $DNSNAME
+
+echo "*********************************************************************************************"
+echo "Making curl request now..."
+echo "Response is as belows "
+
+curl $DNSNAME
+
+echo "*********************************************************************************************"
---
+++ itmo-544/module-05/destroy-env.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Dynamically detect your infrastructure and destroy it/terminate it
+
+
+echo "Finding and storing the instance IDs for default region"
+
+# First Describe EC2 instances
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
+
+EC2IDS=$(aws ec2 describe-instances \
+    --output=text \
+    --query='Reservations[*].Instances[*].InstanceId' \
+    --filter Name=instance-state-name,Values=pending,running)
+
+echo "*********************************************************************************************"
+echo ${EC2IDS}
+echo "*********************************************************************************************"
+
+# Finding taget group ARN
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-target-groups.html
+
+echo "Finding and storing the target group ARN for default region"
+
+TGARN=$(aws elbv2 describe-target-groups --output=text --query='TargetGroups[*].TargetGroupArn' --names ${9})
+
+echo "*********************************************************************************************"
+echo "TGARN: $TGARN"
+echo "*********************************************************************************************"
+
+
+# Deregistering attached EC2 IDS before terminating instances
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/deregister-targets.html
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/target-deregistered.html
+
+declare -a IDSARRAY
+IDSARRAY=( $EC2IDS )
+
+echo "*********************************************************************************************"
+echo "Deregistering Targets now..."
+
+for ID in ${IDSARRAY[@]};
+do
+  echo "Deregistering Target with Id: $ID"
+  aws elbv2 deregister-targets \
+    --target-group-arn $TGARN --targets Id=$ID
+  # This usually takes time
+  aws elbv2 wait target-deregistered  --target-group-arn $TGARN --targets Id=$ID
+  echo "Target $ID deregistered"
+done
+
+echo "*********************************************************************************************"
+
+# Deleting target group, and wait for it to deregister
+
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/delete-target-group.html
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/target-deregistered.html
+# First Query to get the ELB name using the --query and --filters
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
+
+
+echo "Deleting Target Group now..."
+
+aws elbv2 delete-target-group --target-group-arn $TGARN
+# Error here because it's already deleted!
+# aws elbv2 wait target-deregistered --target-group-arn $TGARN
+
+echo "Target groups deleted!"
+echo "*********************************************************************************************"
+
+
+# Now Terminate all EC2 instances
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/terminate-instances.html
+
+echo "Terminating instances now..."
+
+aws ec2 terminate-instances --instance-ids $EC2IDS
+
+echo "Waiting for instances to be terminated..." 
+aws ec2 wait instance-terminated --instance-ids $EC2IDS
+echo "Instances are terminated!"
+
+echo "*********************************************************************************************"
+
+echo "Finding and storing the ELB ARNS for default region"
+
+ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
+
+echo "*********************************************************************************************"
+echo $ELBARN
+echo "*********************************************************************************************"
+
+# First Query to get the ELB name using the --query and --filters
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
+
+echo "Finding and storing the Listener ARN for default region"
+
+LISTARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELBARN --output=text --query='Listeners[*].ListenerArn' )
+
+echo "*********************************************************************************************"
+echo $LISTARN
+echo "*********************************************************************************************"
+
+echo "Deleting listener now..."
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/delete-listener.html
+
+aws elbv2 delete-listener --listener-arn $LISTARN
+
+echo "Listeners deleted!"
+echo "*********************************************************************************************"
+
+# Delete loadbalancer
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/delete-load-balancer.html
+
+echo "Deleting load-balancers now..."
+
+aws elbv2 delete-load-balancer --load-balancer-arn $ELBARN
+
+echo "Waiting for load-balancer to be deleted..."
+aws elbv2 wait load-balancers-deleted --load-balancer-arns $ELBARN
+echo "Load balancers deleted!"
+
+# Can we delete multiple load-balancers using this command?
+
+# for ELBARN in $ELBARNS;
+# do
+#     echo $ELBARN
+#     aws elbv2 delete-load-balancer --load-balancer-arn $ELBARN
+# done
