685497bbe008a72e054b7783fb670ab03725fabe
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-10-15 12:16:33 -0500

    updated destroy script

--- itmo-544/module-04/destroy-env.sh
+++ itmo-544/module-04/destroy-env.sh
@@ -2,7 +2,6 @@
 
 # Dynamically detect your infrastructure and destroy it/terminate it
 
-
 echo "Finding and storing the instance IDs for default region"
 
 # First Describe EC2 instances
@@ -16,6 +15,7 @@ EC2IDS=$(aws ec2 describe-instances \
 echo "*********************************************************************************************"
 echo ${EC2IDS}
 echo "*********************************************************************************************"
+
 
 # Finding taget group ARN
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-target-groups.html
@@ -46,11 +46,41 @@ do
   aws elbv2 deregister-targets \
     --target-group-arn $TGARN --targets Id=$ID
   # This usually takes time
-  aws elbv2 wait target-deregistered  --target-group-arn $TGARN --targets Id=$ID
+  aws elbv2 wait target-deregistered  --target-group-arn $TGARN --targets Id=$ID,Port=80
   echo "Target $ID deregistered"
 done
 
 echo "*********************************************************************************************"
+
+
+echo "Finding and storing the ELB ARNS for default region"
+
+ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
+
+echo "*********************************************************************************************"
+echo $ELBARN
+echo "*********************************************************************************************"
+
+# First Query to get the ELB name using the --query and --filters
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
+
+echo "Finding and storing the Listener ARN for default region"
+
+LISTARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELBARN --output=text --query='Listeners[*].ListenerArn' )
+
+echo "*********************************************************************************************"
+echo $LISTARN
+echo "*********************************************************************************************"
+
+
+echo "Deleting listener now..."
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/delete-listener.html
+
+aws elbv2 delete-listener --listener-arn $LISTARN
+
+echo "Listeners deleted!"
+echo "*********************************************************************************************"
+
 
 # Deleting target group, and wait for it to deregister
 
@@ -83,32 +113,6 @@ echo "Instances are terminated!"
 
 echo "*********************************************************************************************"
 
-echo "Finding and storing the ELB ARNS for default region"
-
-ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
-
-echo "*********************************************************************************************"
-echo $ELBARN
-echo "*********************************************************************************************"
-
-# First Query to get the ELB name using the --query and --filters
-# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
-
-echo "Finding and storing the Listener ARN for default region"
-
-LISTARN=$(aws elbv2 describe-listeners --load-balancer-arn $ELBARN --output=text --query='Listeners[*].ListenerArn' )
-
-echo "*********************************************************************************************"
-echo $LISTARN
-echo "*********************************************************************************************"
-
-echo "Deleting listener now..."
-# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/delete-listener.html
-
-aws elbv2 delete-listener --listener-arn $LISTARN
-
-echo "Listeners deleted!"
-echo "*********************************************************************************************"
 
 # Delete loadbalancer
 # https://docs.aws.amazon.com/cli/latest/reference/elbv2/delete-load-balancer.html
@@ -120,6 +124,7 @@ aws elbv2 delete-load-balancer --load-balancer-arn $ELBARN
 echo "Waiting for load-balancer to be deleted..."
 aws elbv2 wait load-balancers-deleted --load-balancer-arns $ELBARN
 echo "Load balancers deleted!"
+
 
 # Can we delete multiple load-balancers using this command?
 
