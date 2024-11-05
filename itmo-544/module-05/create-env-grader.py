c63030ca3899920f979d75e00b4d50c399381caf
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-11-05 09:43:56 -0600

    understood python dict syntax

--- itmo-544/module-05/create-env-grader.py
+++ itmo-544/module-05/create-env-grader.py
@@ -7,7 +7,7 @@ ec2 = boto3.client('ec2')
 print("Checking Launch Templates")
 response = ec2.describe_launch_templates()
 print(response)
-print(response.'LaunchTemplates')
+print(response['LaunchTemplates'])
 
 autoscaling = boto3.client('autoscaling')
 
@@ -16,7 +16,7 @@ autoscaling = boto3.client('autoscaling')
 print("Checking Auto Scaling Groups")
 response = autoscaling.describe_auto_scaling_groups()
 print(response)
-print(response.AutoScalingGroups[0].AutoScalingGroupName)
+print(response[AutoScalingGroups[0][AutoScalingGroupName]])
 
 # Describe Load Balancer
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/autoscaling/client/describe_load_balancers.html
@@ -39,4 +39,4 @@ response = ec2.describe_instances(
     ],
 )
 print(response)
-print(response.Reservations[0].Instances)
+print(response[Reservations[0][Instances]])
