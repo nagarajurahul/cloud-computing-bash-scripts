c30005ae8c9a0a9c4d46095aa7759a07b5a29d6c
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-11-05 09:19:48 -0600

    updated comments

--- itmo-544/module-05/create-env-grader.py
+++ itmo-544/module-05/create-env-grader.py
@@ -4,14 +4,20 @@ ec2 = boto3.client('ec2')
 
 # Describe Launch Templates
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/describe_launch_templates.html
-
-response = ec2.describe_launch_templates(
-)
+print("Checking Launch Templates")
+response = ec2.describe_launch_templates()
+print(response)
 
 autoscaling = boto3.client('autoscaling')
 
 # Describe Auto Scaling Groups
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/autoscaling/client/describe_auto_scaling_groups.html
+print("Checking Auto Scaling Groups")
 response = client.describe_auto_scaling_groups()
+print(response)
 
+# Describe Load Balancer
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/autoscaling/client/describe_load_balancers.html
+print("Checking Load Balancers")
 response = autoscaling.describe_load_balancers()
+print(response)
