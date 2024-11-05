a1fefbc1f1ddffd33ec242536b7fec6af7fc01d2
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-11-05 09:12:57 -0600

    comments added for LT

--- itmo-544/module-05/create-env-grader.py
+++ itmo-544/module-05/create-env-grader.py
@@ -2,5 +2,8 @@ import boto3
 
 ec2 = boto3.client('ec2')
 
+# Describe Launch Templates
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/describe_launch_templates.html
+
 response = ec2.describe_launch_templates(
 )
