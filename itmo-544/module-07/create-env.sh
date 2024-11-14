63aac51cd3ce9ed7ff04a32a358ceb2663092aed
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-11-14 10:41:59 -0600

    udpated the flow in create-env script

--- itmo-544/module-07/create-env.sh
+++ itmo-544/module-07/create-env.sh
@@ -27,26 +27,29 @@
 
 
 
+# Storing the instance id here...
+
 # https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-instances.html
 
-EC2IDS=$(aws ec2 describe-instances \
-    --output=text \
-    --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running)
+# EC2IDS=$(aws ec2 describe-instances \
+#     --output=text \
+#     --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running)
 
-echo "Finding and storing the Instance IDs"
-echo "*********************************************************************************************"
-echo $EC2IDS
-echo "*********************************************************************************************"
+# echo "Finding and storing the Instance IDs"
+# echo "*********************************************************************************************"
+# echo $EC2IDS
+# echo "*********************************************************************************************"
 
 
-# https://docs.aws.amazon.com/cli/latest/reference/ec2/create-image.html
+# # https://docs.aws.amazon.com/cli/latest/reference/ec2/create-image.html
 
-echo "Creating our custom AMI now..."
+# echo "Creating our custom AMI now..."
 
-aws ec2 create-image \
-    --instance-id $EC2IDS \
-    --name "My EC2 Server" \
-    --description "An AMI for my server"
+# aws ec2 create-image \
+#     --instance-id $EC2IDS \
+#     --name "My EC2 Server" \
+#     --description "An AMI for my server" \
+#     --tag-specifications "ResourceType=image,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=module-07}]"
 
 # Replace the ami in the arguments with this new AMI
 
@@ -56,25 +59,30 @@ aws ec2 create-image \
 
 echo "Finding and storing our Custom AMI..."
 
-aws ec2 describe-images \
-    --filters "Name=tag:Type,Values=Custom" \
+AMI = $(aws ec2 describe-images \
+    --filters "Name=tag:name,Values=module-07" \
     --query 'Images[*].[ImageId]' \
-    --output text
+    --output text)
 
 
-
+echo "Finding and storing our custom AMI"
 echo "*********************************************************************************************"
-echo "Creating EC2 instances now with our custom AMI..."
+echo $AMI
+echo "*********************************************************************************************"
 
-aws ec2 run-instances \
-    --image-id ${1} \
-    --instance-type ${2} \
-    --key-name ${3} \
-    --security-group-ids ${4} \
-    --count ${5} \
-    --user-data file://${6} \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=module-07}]' \
-    --placement "AvailabilityZone=${7}"
+
+# echo "*********************************************************************************************"
+# echo "Creating EC2 instances now with our custom AMI..."
+
+# aws ec2 run-instances \
+#     --image-id ${1} \
+#     --instance-type ${2} \
+#     --key-name ${3} \
+#     --security-group-ids ${4} \
+#     --count ${5} \
+#     --user-data file://${6} \
+#     --tag-specifications 'ResourceType=instance,Tags=[{Key=course,Value=itmo-544}, {Key=name,Value=module-07}]' \
+#     --placement "AvailabilityZone=${7}"
 
 
 
