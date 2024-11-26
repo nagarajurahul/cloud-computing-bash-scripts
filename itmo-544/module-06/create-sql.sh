9120aaa085392962207d7e76b932ae3df0686adc
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-11-26 09:32:10 -0600

    updated sql script

--- itmo-544/module-06/create-sql.sh
+++ itmo-544/module-06/create-sql.sh
@@ -14,18 +14,56 @@
 # By default, MySQL listens on port 3306
 # Feel free to add tags if required
 
+
+echo "*********************************************************************************************"
+echo "Finding and storing the subnet IDs for availability zones defined in arguments"
+
+SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${10}")
+SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${11}")
+SUBNET2C=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
+
+echo "*********************************************************************************************"
+echo $SUBNET2A
+echo $SUBNET2B
+echo $SUBNET2C
+echo "*********************************************************************************************"
+
+
+echo "Creating db subnet groups now..."
+
+aws rds create-db-subnet-group \
+    --db-subnet-group-name "${19}-subnet-group" \
+    --db-subnet-group-description "My DB Subnet Group" \
+    --subnet-ids $SUBNET2A $SUBNET2B $SUBNET2C
+
+echo "*********************************************************************************************"
+
+
+echo "Pulling db security groups now..."
+
+RDS_SG=$(aws ec2 describe-security-groups \
+    --filters "Name=group-name,Values=sql-security-group" \
+    --query "SecurityGroups[0].GroupId" \
+    --output text)
+
+echo "*********************************************************************************************"
+echo "RDS_SG: $RDS_SG"
+echo "*********************************************************************************************"
+
 echo "Creating db instances now..."
 
 aws rds create-db-instance \
     --db-instance-identifier ${19} \
     --db-instance-class db.t3.micro \
     --engine mysql \
-    --allocated-storage 20 \
+    --allocated-storage 10 \
     --tags Key=Name,Value=module-06 \
     --vpc-security-group-ids sg-0d0101b597cd9ec5e \
-    --db-subnet-group mydbsubnetgroup \
+    --db-subnet-group "${19}-subnet-group" \
     --master-username controller \
     --manage-master-user-password
+
+echo "*********************************************************************************************"
 
 echo "Waiting for db instances to be available"
 
@@ -34,6 +72,8 @@ aws rds wait \
     --db-instance-identifier ${19} 
 
 echo "DB Instance is now running..."
+echo "*********************************************************************************************"
+
 
 (aws rds describe-db-instances --query='DBInstances[*].DBInstanceIdentifier')
 
