f912fc11db6e8b3ffb2d55f9b1dce6d3a2e042ba
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-11-28 22:42:16 -0600

    updated create grader script

--- itmo-544/module-06/create-env-grader.py
+++ itmo-544/module-06/create-env-grader.py
@@ -31,70 +31,73 @@ def currentPoints():
 #     currentPoints()
 
 
-
-# Describe Launch Templates
-# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/describe_launch_templates.html
-print('*' * 79)
-print("Checking Launch Templates...")
-response = ec2.describe_launch_templates()
-print(response)
-# print(response['LaunchTemplates'])
-
-print("The number of launch templates are: " + str(len(response['LaunchTemplates'])))
-
-if len(response['LaunchTemplates']) >= 1:
-    print("Correct answer you have:" + str(len(response['LaunchTemplates'])) + " Launch Templates...")
-    grandtotal += 1
-    currentPoints()
-else:
-    print("You have  an incorrect number of Launch Templates: " + str(len(response['LaunchTemplates'])) + ", perhaps check if you have created the Launch Templates...")
-    currentPoints()
+# Commenting from here
 
 
+# # Describe Launch Templates
+# # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/describe_launch_templates.html
+# print('*' * 79)
+# print("Checking Launch Templates...")
+# response = ec2.describe_launch_templates()
+# print(response)
+# # print(response['LaunchTemplates'])
 
-# Describe Auto Scaling Groups
-# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/autoscaling/client/describe_auto_scaling_groups.html
-print('*' * 79)
-print("Checking Auto Scaling Groups...")
-response = autoscaling.describe_auto_scaling_groups()
-print(response)
+# print("The number of launch templates are: " + str(len(response['LaunchTemplates'])))
 
-print("The number of Auto Scaling Groups are: " + str(len(response['AutoScalingGroups'])))
-
-if len(response['AutoScalingGroups']) >= 1:
-
-    asgname=(response['AutoScalingGroups'][0]['AutoScalingGroupName'])
-    print(asgname)
-    print(type(asgname))
-
-    print("Correct answer you have:" + str(len(response['AutoScalingGroups'])) + " Auto Scaling Groups...")
-    grandtotal += 1
-    currentPoints()
-else:
-    print("You have  an incorrect number of Auto Scaling Groups: " + str(len(response['AutoScalingGroups'])) + ", perhaps check if you have created Auto Scaling Groups...")
-    currentPoints()
+# if len(response['LaunchTemplates']) >= 1:
+#     print("Correct answer you have:" + str(len(response['LaunchTemplates'])) + " Launch Templates...")
+#     grandtotal += 1
+#     currentPoints()
+# else:
+#     print("You have  an incorrect number of Launch Templates: " + str(len(response['LaunchTemplates'])) + ", perhaps check if you have created the Launch Templates...")
+#     currentPoints()
 
 
 
-# Describe Load Balancer
-# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/autoscaling/client/describe_load_balancers.html
-print('*' * 79)
-print("Checking Load Balancers...")
-response = elbv2.describe_load_balancers()
-responseELB=response
-print(response)
+# # Describe Auto Scaling Groups
+# # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/autoscaling/client/describe_auto_scaling_groups.html
+# print('*' * 79)
+# print("Checking Auto Scaling Groups...")
+# response = autoscaling.describe_auto_scaling_groups()
+# print(response)
 
-print("The number of Load Balancers are: " + str(len(response['LoadBalancers'])))
+# print("The number of Auto Scaling Groups are: " + str(len(response['AutoScalingGroups'])))
 
-if len(response['LoadBalancers']) >= 1:
-    print("Correct answer you have:" + str(len(response['LoadBalancers'])) + " Load Balancers...")
-    grandtotal += 1
-    currentPoints()
-else:
-    print("You have  an incorrect number of Load Balancers: " + str(len(response['LoadBalancers'])) + ", perhaps check if you have created Load Balancers...")
-    currentPoints()
+# if len(response['AutoScalingGroups']) >= 1:
+
+#     asgname=(response['AutoScalingGroups'][0]['AutoScalingGroupName'])
+#     print(asgname)
+#     print(type(asgname))
+
+#     print("Correct answer you have:" + str(len(response['AutoScalingGroups'])) + " Auto Scaling Groups...")
+#     grandtotal += 1
+#     currentPoints()
+# else:
+#     print("You have  an incorrect number of Auto Scaling Groups: " + str(len(response['AutoScalingGroups'])) + ", perhaps check if you have created Auto Scaling Groups...")
+#     currentPoints()
 
 
+
+# # Describe Load Balancer
+# # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/autoscaling/client/describe_load_balancers.html
+# print('*' * 79)
+# print("Checking Load Balancers...")
+# response = elbv2.describe_load_balancers()
+# responseELB=response
+# print(response)
+
+# print("The number of Load Balancers are: " + str(len(response['LoadBalancers'])))
+
+# if len(response['LoadBalancers']) >= 1:
+#     print("Correct answer you have:" + str(len(response['LoadBalancers'])) + " Load Balancers...")
+#     grandtotal += 1
+#     currentPoints()
+# else:
+#     print("You have  an incorrect number of Load Balancers: " + str(len(response['LoadBalancers'])) + ", perhaps check if you have created Load Balancers...")
+#     currentPoints()
+
+
+# Commenting till here
 
 # Describe EC2 instances
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2/client/describe_instances.html
@@ -105,7 +108,7 @@ response = ec2.describe_instances(
         {
             'Name': 'tag:Name',
             'Values': [
-                'module-05',
+                'module-06',
             ],
         },
         {
