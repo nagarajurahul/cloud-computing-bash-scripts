763dc7d34a9154d60bc30f39d29094db1cdbc117
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-11-30 15:22:48 -0600

    added s3 logic

--- itmo-544/module-07/add-up.sh
+++ itmo-544/module-07/add-up.sh
@@ -27,3 +27,26 @@ aws rds wait db-instance-available \
     --db-instance-identifier ${19}
 
 echo "RDS instance ${19} created successfully from snapshot $RDS_SNAPSHOT."
+
+
+REGION=$("us-east-1")
+
+if aws s3api head-bucket --bucket "${21}"; then
+    echo "Bucket ${21} exists."
+else
+    echo "Bucket ${21} does not exist."
+    echo "Creating Bucket ${21} in Region $REGION"
+    aws s3api create-bucket --bucket "${21}" --region "$REGION" \
+            --create-bucket-configuration LocationConstraint="$REGION"
+        if [ $? -eq 0 ]; then
+            echo "Bucket ${21} created successfully."
+        else
+            echo "Failed to create bucket ${21}."
+        fi
+fi
+
+
+
+# Verify buckets were created
+echo "Listing all S3 buckets..."
+aws s3api list-buckets --query "Buckets[].Name"
