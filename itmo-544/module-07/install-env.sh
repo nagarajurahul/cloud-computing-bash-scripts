66d0ab0baaf0e3ea21a9ddeda2f2a4e24c3af9a2
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-12-01 01:54:55 -0600

    updated commands

--- itmo-544/module-07/install-env.sh
+++ itmo-544/module-07/install-env.sh
@@ -8,6 +8,8 @@ sudo apt install -y nginx
 # Get your source code (index.html and app.js) on to each EC2 instance
 # So we can serve the provided index.html not the default "welcome to Nginx"
 
+cd /home/ubuntu
+
 # Change URL to your private repo
 sudo -u ubuntu git clone git@github.com:illinoistech-itm/rnagaraju.git
 
