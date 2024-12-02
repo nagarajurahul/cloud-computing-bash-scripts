ee98957d26e02f471b06a3cc7977ff5408c81a86
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-12-02 10:55:07 -0600

    updated to automatically fetch launch template versions

--- itmo-544/module-09/extras/create-launch-template.sh
+++ itmo-544/module-09/extras/create-launch-template.sh
@@ -9,12 +9,23 @@ aws ec2 create-launch-template-version \
     --launch-template-data file://config.json
 
 echo "*********************************************************************************************"
+echo "Fetching the latest launch template version number..."
+
+# Fetch the latest version number using AWS CLI and jq to parse the JSON output
+LATEST_VERSION=$(aws ec2 describe-launch-template-versions \
+    --launch-template-name ${15} \
+    --query 'LaunchTemplateVersions[-1].VersionNumber' \
+    --output text)
+
+echo "Latest version is: $LATEST_VERSION"
+
+echo "*********************************************************************************************"
 echo "Modifying launch template version to default now..."
 
-
+# Set the latest version as the default
 aws ec2 modify-launch-template \
     --launch-template-name ${15} \
-    --default-version 2
+    --default-version $LATEST_VERSION
 
 echo "*********************************************************************************************"
 echo "Pulling launch-template now..."
