0bd2d317301a2a6cfc2e6645c0952a857129df3c
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-12-01 22:43:07 -0600

    added module-09 folder

---
+++ itmo-544/module-09/create-env.sh
@@ -0,0 +1,409 @@
+#!/bin/bash
+
+# Finding subnets
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-subnets.html
+
+echo "*********************************************************************************************"
+echo "Finding and storing the subnet IDs for availability zones defined in arguments"
+
+SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${10}")
+SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${11}")
+SUBNET2C=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
+
+echo "*********************************************************************************************"
+echo $SUBNET2A
+echo $SUBNET2B
+echo $SUBNET2C
+echo "*********************************************************************************************"
+
+
+# Create launch template
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/create-launch-template.html
+
+echo "*********************************************************************************************"
+echo "Creating launch-template now..."
+
+aws ec2 create-launch-template \
+    --launch-template-name ${15} \
+    --version-description version1 \
+    --launch-template-data file://config.json
+
+
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/create-load-balancer.html
+
+echo "*********************************************************************************************"
+echo "Creating load-balancer now..."
+
+# Need to remove sg here
+
+aws elbv2 create-load-balancer \
+    --name ${8} \
+    --subnets $SUBNET2A $SUBNET2B $SUBNET2C \
+    --tags Key=name,Value=${13} \
+    --security-groups ${4} \
+    --output table
+
+#    --security-groups ${20}\
+#     --scheme internal \  -> to make it internal LB
+
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-listeners.html
+
+echo "*********************************************************************************************"
+echo "Finding and storing the ELB ARN for default region"
+
+ELBARN=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].LoadBalancerArn')
+
+echo "*********************************************************************************************"
+echo "Printing ELBARN: $ELBARN"
+echo "*********************************************************************************************"
+
+
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/wait/
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/wait/load-balancer-available.html
+
+echo "*********************************************************************************************"
+echo "Waiting for ELB to become available..."
+aws elbv2 wait load-balancer-available --load-balancer-arns $ELBARN
+echo "ELB is available..."
+
+
+echo "*********************************************************************************************"
+echo "Creating target groups now..."
+
+# Find the VPC
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-vpcs.html
+
+MYVPCID=$(aws ec2 describe-vpcs --output=text --query='Vpcs[*].VpcId')
+
+# Create the target group - this is application load balancer type
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/create-target-group.html
+
+aws elbv2 create-target-group \
+    --name ${9} \
+    --protocol HTTP \
+    --port 80 \
+    --target-type instance \
+    --vpc-id $MYVPCID
+
+# Describe the target groups and get target ARNs
+# https://docs.aws.amazon.com/cli/latest/reference/elbv2/describe-target-groups.html
+
+echo "*********************************************************************************************"
+echo "Finding and storing target group ARN"
+
+TGARN=$(aws elbv2 describe-target-groups --output=text --query='TargetGroups[*].TargetGroupArn' --names ${9})
+
+echo "*********************************************************************************************"
+echo "Target group ARN: $TGARN"
+echo "*********************************************************************************************"
+
+
+echo "*********************************************************************************************"
+echo "Creating listeners now..."
+
+# Creating listener
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/elbv2/create-listener.html
+
+aws elbv2 create-listener \
+    --load-balancer-arn $ELBARN \
+    --protocol HTTP \
+    --port 80 \
+    --default-actions Type=forward,TargetGroupArn=$TGARN
+
+echo "*********************************************************************************************"
+echo "Listeners are up!"
+
+
+echo "*********************************************************************************************"
+echo "Creating AutoScaling Group now..."
+
+# Create auto-scaling groups
+# https://docs.aws.amazon.com/cli/latest/reference/autoscaling/create-auto-scaling-group.html
+aws autoscaling create-auto-scaling-group \
+    --auto-scaling-group-name ${14} \
+    --launch-template LaunchTemplateName=${15} \
+    --target-group-arns $TGARN \
+    --health-check-type ELB \
+    --health-check-grace-period 120 \
+    --min-size ${16} \
+    --max-size ${17} \
+    --desired-capacity ${18}
+
+sleep 5
+
+EC2IDS=$(aws ec2 describe-instances \
+    --output=text \
+    --query='Reservations[*].Instances[*].InstanceId' --filter Name=instance-state-name,Values=pending,running)
+
+echo "*********************************************************************************************"
+echo "Finding and storing the Instance IDs"
+echo "*********************************************************************************************"
+echo $EC2IDS
+echo "*********************************************************************************************"
+
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/
+#https://docs.aws.amazon.com/cli/latest/reference/ec2/wait/instance-running.html
+
+echo "*********************************************************************************************"
+echo "Waiting for instances..."
+aws ec2 wait instance-running --instance-ids $EC2IDS
+echo "Instances are up!"
+
+echo "*********************************************************************************************"
+DNSNAME=$(aws elbv2 describe-load-balancers --output=text --query='LoadBalancers[*].DNSName')
+DNSNAME="http://$DNSNAME"
+
+echo "Kindly curl using the below link"
+echo $DNSNAME
+
+echo "*********************************************************************************************"
+echo "Making curl request now..."
+sleep 5
+echo "Response is as belows "
+
+curl $DNSNAME
+
+echo "*********************************************************************************************"
+
+
+
+# New scripts add below
+
+
+
+# Create DB Instance for MySQL using AWS RDS
+
+# References
+
+# https://docs.aws.amazon.com/cli/latest/reference/rds/#cli-aws-rds
+# https://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+
+# First 3 options are mandatory
+# db-instance-identifier - should be unique
+# db.t3.micro, no-multi-az and 20 for storage - free-tier
+# Please make sure, these are the same values for free-tier
+# By default, MySQL listens on port 3306
+# Feel free to add tags if required
+
+
+# echo "*********************************************************************************************"
+# echo "Finding and storing the subnet IDs for availability zones defined in arguments"
+
+# SUBNET2A=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${10}")
+# SUBNET2B=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${11}")
+# SUBNET2C=$(aws ec2 describe-subnets --output=text --query='Subnets[*].SubnetId' --filter "Name=availability-zone,Values=${12}")
+
+# echo "*********************************************************************************************"
+# echo $SUBNET2A
+# echo $SUBNET2B
+# echo $SUBNET2C
+# echo "*********************************************************************************************"
+
+
+# echo "Creating db subnet groups now..."
+
+# aws rds create-db-subnet-group \
+#     --db-subnet-group-name "${19}-subnet-group" \
+#     --db-subnet-group-description "My DB Subnet Group" \
+#     --subnet-ids $SUBNET2A $SUBNET2B $SUBNET2C
+
+# echo "*********************************************************************************************"
+# echo "Pulling db security groups now..."
+
+# RDS_SG=$(aws ec2 describe-security-groups \
+#     --filters "Name=group-name,Values=sql-security-group" \
+#     --query "SecurityGroups[0].GroupId" \
+#     --output text)
+
+# echo "*********************************************************************************************"
+# echo "RDS_SG: $RDS_SG"
+# echo "*********************************************************************************************"
+
+# echo "*********************************************************************************************"
+# echo "Creating db instances now..."
+
+# aws rds create-db-instance \
+#     --db-instance-identifier ${19} \
+#     --db-instance-class db.t3.micro \
+#     --engine mysql \
+#     --allocated-storage 10 \
+#     --tags Key=Name,Value=module-06 \
+#     --vpc-security-group-ids $RDS_SG \
+#     --db-subnet-group "${19}-subnet-group" \
+#     --master-username controller \
+#     --manage-master-user-password
+
+
+# echo "*********************************************************************************************"
+# echo "Waiting for db instances to be available"
+
+# aws rds wait db-instance-available \
+#     --db-instance-identifier ${19}
+ 
+# # aws rds wait db-instance-available \
+# #     --db-instance-identifier rndbinstance
+
+
+# echo "DB Instance is now running..."
+# echo "*********************************************************************************************"
+
+
+#     # --port 3306 \
+#     # --backup-retention-period 7 \
+#     # --no-multi-az \
+#     # --availability-zone us-east-2a \
+#     # --master-username admin \
+#     # --master-user-password admin \
+
+
+# Added new script for creating RDS from snapshot
+
+echo "Creating db subnet groups now..."
+
+aws rds create-db-subnet-group \
+    --db-subnet-group-name "${19}-subnet-group" \
+    --db-subnet-group-description "My DB Subnet Group" \
+    --subnet-ids $SUBNET2A $SUBNET2B $SUBNET2C
+
+echo "*********************************************************************************************"
+
+
+echo "Pulling db security groups now..."
+
+RDS_SG=$(aws ec2 describe-security-groups \
+    --filters "Name=group-name,Values=sql-security-group" \
+    --query "SecurityGroups[0].GroupId" \
+    --output text)
+
+echo "*********************************************************************************************"
+echo "RDS_SG: $RDS_SG"
+echo "*********************************************************************************************"
+
+
+
+echo "*********************************************************************************************"
+echo "Pulling snapshots now..."
+
+# Get the DB Snapshot identifier dynamically from AWS CLI
+SNAPSHOT_ID=$(aws rds describe-db-snapshots \
+    --query "DBSnapshots[?Status=='available'] | [-1].DBSnapshotIdentifier" \
+    --output text)
+
+echo "*********************************************************************************************"
+echo "Using Snapshot: $SNAPSHOT_ID"
+echo "*********************************************************************************************"
+
+# Ensure we are getting a valid snapshot
+if [ "$SNAPSHOT_ID" == "None" ]; then
+    echo "No available snapshot found!"
+    exit 1
+fi
+
+
+
+
+# Set the new DB instance identifier
+DB_INSTANCE_ID="${19}"
+
+# Create the new DB instance from the snapshot (Free Tier eligible settings)
+echo "*********************************************************************************************"
+echo "Creating a new DB instance from the snapshot now..."
+
+# aws rds create-db-instance \
+#     --db-instance-identifier $DB_INSTANCE_ID \
+#     --db-instance-class db.t3.micro \
+#     --engine mysql \
+#     --db-snapshot-identifier $SNAPSHOT_ID \
+#     --allocated-storage 10 \
+#     --tags Key=Name,Value=module-07 \
+#     --vpc-security-group-ids $RDS_SG \
+#     --db-subnet-group "${19}-subnet-group" \
+#     --master-username controller \
+#     --manage-master-user-password
+
+aws rds restore-db-instance-from-db-snapshot \
+    --db-instance-identifier $DB_INSTANCE_ID \
+    --db-snapshot-identifier $SNAPSHOT_ID \
+    --db-instance-class db.t3.micro \
+    --tags Key=Name,Value=module-08 \
+    --vpc-security-group-ids $RDS_SG \
+    --db-subnet-group "${19}-subnet-group"
+
+echo "*********************************************************************************************"
+echo "Waiting for the DB instance to be available..."
+
+# Wait for the DB instance to be available
+aws rds wait db-instance-available \
+    --db-instance-identifier $DB_INSTANCE_ID
+
+echo "*********************************************************************************************"
+aws rds modify-db-instance \
+    --db-instance-identifier $DB_INSTANCE_ID \
+    --manage-master-user-password
+
+
+echo "*********************************************************************************************"
+echo "DB Instance is now running and created from snapshot!"
+echo "*********************************************************************************************"
+
+
+
+# S3 Script
+
+# Function to check if an S3 bucket exists and create it if it does not
+create_bucket_if_not_exists() {
+    local BUCKET_NAME=$1
+    local REGION=$2
+
+    echo "Checking if bucket $BUCKET_NAME exists..."
+    if aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
+        echo "Bucket $BUCKET_NAME exists."
+    else
+        echo "Bucket $BUCKET_NAME does not exist."
+
+        echo "*********************************************************************************************"
+
+        echo "Creating bucket $BUCKET_NAME in region $REGION..."
+        
+        # Use LocationConstraint for regions other than us-east-1
+        if [ "$REGION" == "us-east-1" ]; then
+            aws s3api create-bucket --bucket "$BUCKET_NAME"
+        else
+            aws s3api create-bucket --bucket "$BUCKET_NAME" --region "$REGION" \
+                --create-bucket-configuration LocationConstraint="$REGION"
+        fi
+        
+        echo "*********************************************************************************************"
+        if [ $? -eq 0 ]; then      
+            echo "Bucket $BUCKET_NAME created successfully."
+        else
+            echo "Failed to create bucket $BUCKET_NAME."
+        fi
+    fi
+}
+
+# Specify region
+REGION="us-east-2"
+
+# Use arguments for bucket names
+FIRST_BUCKET_NAME=${21}
+SECOND_BUCKET_NAME=${22}
+
+echo "*********************************************************************************************"
+create_bucket_if_not_exists "$FIRST_BUCKET_NAME" "$REGION"
+
+echo "*********************************************************************************************"
+create_bucket_if_not_exists "$SECOND_BUCKET_NAME" "$REGION"
+
+echo "*********************************************************************************************"
+# Verify buckets were created
+echo "Listing all S3 buckets..."
+aws s3api list-buckets --query "Buckets[].Name"
+
+
+
+aws s3api put-public-access-block --bucket ${21} \
+    --public-access-block-configuration "BlockPublicPolicy=false"
+    
+aws s3api put-bucket-policy --bucket ${21} \
+    --policy file://raw-bucket-policy.json
---
+++ itmo-544/module-09/lab.md
@@ -0,0 +1,100 @@
+# Module 08
+
+## Objectives
+
+* Demonstrate the process to install AWS JavaScript SDK packages
+* Demonstrate the concept of deploying a working cloud native application
+* Demonstrate the IAM concepts needed for integrating cloud services for an application
+* Demonstrate the termination of cloud resources in an automated fashion
+
+## Links to Use
+
+* [AWS JavaScript SDK Reference](https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/ "AWS JavaScript SDK Reference")
+* [AWS JavaScript SDK Installation](https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html "AWS JavaScript SDK Installation")
+* [NodeJS](https://nodejs.org/en "NodeJS")
+* [Installing Node JS on Ubuntu](https://github.com/nodesource/distributions#using-ubuntu-nodejs-20 "Installing Node JS on Ubuntu")
+* [PM2](https://pm2.keymetrics.io/ "PM2 service manager for JavaScript applications")
+* [Python Boto3 AWS SDK](https://boto3.amazonaws.com/v1/documentation/api/latest/index.html "Python Boto3 AWS SDK")
+
+## Arguments.txt
+
+```bash
+# ${1} ami-085f9c64a9b75eed5
+# ${2} t2.micro
+# ${3} itmo-544-2024
+# ${4} sg-0c7709a929dbfbb4d
+# ${5} 3
+# ${6} install-env.sh
+# ${7} us-east-2a
+# ${8} jrh-elb
+# ${9} jrh-tg
+# ${10} us-east-2a
+# ${11} us-east-2b
+# ${12} us-east-2c
+# ${13} module-08
+# ${14} asg name
+# ${15} launch-template name
+# ${16} asg min
+# ${17} asg max
+# ${18} asg desired
+# ${19} RDS Database Instance Identifier (no punctuation) --db-instance-identifier
+# ${20} IAM Instance Profile Name
+# ${21} S3 Bucket Raw
+# ${22} S3 Bucket Finished
+```
+
+## Assumptions
+
+Assume all the requirements from module-07 to be completed and this module's requirements will add on to module-07. You will need to have IAM permissions for Secrets Manager, RDS, and S3.
+
+## Part 1
+
+Add to your `install-env.sh` logic needed to:
+
+* Remove the Nginx install code
+* Install `Node.js` and all its pre-reqs
+* Install via `npm` all AWS JavaScript SDK packages required
+* Install via `npm` all application dependencies
+* Clone your private repo and using the PM2 process manager start the app.js application
+  * Making use of an Nginx reverse proxy (code provided)
+
+Make use of this code block after your RDS instance launches and after the waiter is complete.
+
+```bash
+aws rds modify-db-instance \
+    --db-instance-identifier ${20} \
+    --manage-master-user-password
+```
+
+## Part 2
+
+Using the [Python Boto3 AWS SDK](https://boto3.amazonaws.com/v1/documentation/api/latest/index.html "Python Boto3 AWS SDK") you will write an autograder script for this project. One for the `create-env.sh` named: `create-env-grader.py`, and one for the `destroy-env.py` named: `destroy-env-grader.py`. Print out information per item below to the screen and keep a grandtotal to print out your score out of 5.
+
+For the `create-env-grader.py`:
+
+* Check for the existence of one RDS instance~~s~~ and one RDS Snapshot
+* Check for the existence of the **module-08** tag for the database instance
+* Check for the existence of two S3 buckets
+* Check for the existence of 2 images named: `vegeta.jpg` and `knuth.jpg`
+* Check for the ELB to return an HTTP 200 upon request
+
+For the `destroy-env-grader.py`:
+
+* Check for the existence of zero Launch Templates
+* Check for the existence of zero S3 buckets
+* Check for the existence of zero ELBs
+* Check for the existence of zero EC2 instances
+* Check for the existence of zero RDS instances
+
+## Deliverables
+
+Create a folder named: `module-08` under your class folder in the provided private repo. Provide the following files:
+
+* `create-env.sh`
+* `destroy-env.sh`
+* `install-env.sh`
+* `create-env-test.py`
+* `destroy-env-test.py`
+  * I will test this script by using my own account information
+
+Submit the URL to the `module-08` folder to Canvas.
