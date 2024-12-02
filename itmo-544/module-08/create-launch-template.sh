53efba571068fcfafa3f7332c6055d1d8e1161d8
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-12-01 20:45:10 -0600

    updated launch template file

--- itmo-544/module-08/create-launch-template.sh
+++ itmo-544/module-08/create-launch-template.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+echo "*********************************************************************************************"
+echo "Creating launch-template now..."
+
 aws ec2 create-launch-template-version \
     --launch-template-name ${15} \
     --version-description version1 \
     --launch-template-data file://config.json
 
+echo "*********************************************************************************************"
+echo "Modifying launch template version to default now..."
+
+
 aws ec2 modify-launch-template \
     --launch-template-name ${15} \
-    --default-version 2
+    --default-version 4
+
+echo "*********************************************************************************************"
+echo "Pulling launch-template now..."
 
 aws ec2 describe-launch-template-versions \
     --launch-template-name ${15}
+
+echo "*********************************************************************************************"
