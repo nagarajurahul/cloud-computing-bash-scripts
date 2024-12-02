9529bd630a1fd5ffce9e772acfd0a4efb81ce072
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-12-02 12:55:52 -0600

    printf updated

--- itmo-544/module-09/destroy-env-grader.py
+++ itmo-544/module-09/destroy-env-grader.py
@@ -184,8 +184,8 @@ try:
     print(f"The number of RDS Instances are: " + str(len(db_instances)))
 
 
-    if len(db_instances) ==0:
-        print("Correct answer you have: {len(db_instances)} RDS instances.")
+    if len(db_instances) == 0:
+        print(f"Correct answer you have: {len(db_instances)} RDS instances.")
         grandtotal += 1
         currentPoints()
     else:
